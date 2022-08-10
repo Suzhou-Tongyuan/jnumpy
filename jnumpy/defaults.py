11196e7e1054f0b372e4b40962394dcb394e6451
songjhaha <songjh96@foxmail.com>
2022-08-10 10:22:48 +0800

    serach julia path in default path before install

--- jnumpy/defaults.py
+++ jnumpy/defaults.py
@@ -21,15 +21,23 @@ def get_julia_exe():
         julia_exepath = get_default_julia_exe()
     return julia_exepath
 
-
-def get_default_julia_exe() -> str:
-    julia_exepath = shutil.which("julia")
+def check_valid_julia_exe(julia_exepath):
     if julia_exepath:
         try:
             ver_cmd = [julia_exepath, "--version"]
             subprocess.check_output(ver_cmd)
         except subprocess.CalledProcessError:
-            julia_exepath = None
+            return None
+    return julia_exepath
+
+def get_default_julia_exe() -> str:
+    # search julia in sys path
+    julia_exepath = shutil.which("julia")
+    julia_exepath = check_valid_julia_exe(julia_exepath)
+    # search julia in default jnumpy path
+    serach_path = get_symlink_dir()
+    julia_exepath = shutil.which("julia", path=serach_path)
+    julia_exepath = check_valid_julia_exe(julia_exepath)
 
     if not julia_exepath:
         quest = "Can not find julia.\nWould you like jnumpy to install julia now?"
@@ -38,7 +46,6 @@ def get_default_julia_exe() -> str:
             raise EnvironmentError("cannot find julia.")
         else:
             setup_julia()
-            serach_path = get_symlink_dir()
             julia_exepath = shutil.which("julia", path=serach_path)
             if not julia_exepath:
                 raise EnvironmentError(
